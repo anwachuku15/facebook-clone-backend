08967747eb84b3d1bf7db9996776deaecd541792
anwachuku15 <anwachuku15@gmail.com>
2020-11-02 11:28:19 -0500

    initialized backed api

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import GridFsStorage from "multer-gridfs-storage";
+import Grid from "gridfs-stream";
+import bodyParser from "body-parser";
+import path from "path";
+import Pusher from "pusher";
+
+Grid.mongo = mongoose.mongo;
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// db config
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+// listen port
+app.listen(port, () => console.log(`listening on localhost:${port}`));
